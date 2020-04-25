81e7f7ce9480315703ce2d71636962903e913040
bharath6365 <bharath6365@gmail.com>
2020-04-25 15:03:25 +0530

    Bring in semantic UI for styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { Button, Header, Icon, Modal } from 'semantic-ui-react';
 import './App.scss';
 import io from 'socket.io-client';
 import Peer from 'simple-peer';
 import styled from 'styled-components';
-import Button from './components/Button';
+import MyButton from './components/Button';
 import UserList from './components/UserList';
 import CallCut from './images/call-cut.png';
 
@@ -27,18 +28,17 @@ const VideoWrapper = styled.div`
 
 const Video = styled.video`
   position: absolute;
-  right: 0; 
+  right: 0;
   bottom: 0;
   height: 100%;
   width: 100%;
   object-fit: cover;
 `;
 
-function App({history}) {
-  
-  const name = localStorage.getItem("name") || sessionStorage.getItem("name");
+function App({ history }) {
+  const name = localStorage.getItem('name') || sessionStorage.getItem('name');
   if (!name) {
-      history.push("/");
+    history.push('/');
   }
 
   // Our ID.
@@ -56,15 +56,15 @@ function App({history}) {
   // Related to Call Accepting.
   const [ caller, setCaller ] = useState({});
   const [ callerSignal, setCallerSignal ] = useState();
-  
+
   // Aren't caller and partner same?
-  const [partner, setPartner] = useState(null);
+  const [ partner, setPartner ] = useState(null);
 
   // Flag to decide whether to accept the call.
   const [ callAccepted, setCallAccepted ] = useState(false);
 
   // Peer object later used to close calls.
-  const [peer, setPeer] = useState(null);
+  const [ peer, setPeer ] = useState(null);
 
   const userVideo = useRef();
   const partnerVideo = useRef();
@@ -100,7 +100,7 @@ function App({history}) {
     // When someone else rejects/disconnects your call.
     socket.current.on('partnerDisconnected', (data) => {
       setPartner(null);
-    })
+    });
 
     /* When someone is calling you this is run or you are being called by someone else. You get the signal of the other user which you must accept. You are not the initiator.
       TODO: Refactor "hey" to a better name
@@ -159,7 +159,7 @@ function App({history}) {
         Connection finally established. You are the initiator and the person you wanted to
         talk with has accepted your request and sent his signal you need to do the handshake.
       */
-     console.log('Call Accepted data is', data);
+      console.log('Call Accepted data is', data);
       peer.signal(data.signal);
       setPartner(data.from);
       setCallAccepted(true);
@@ -189,18 +189,17 @@ function App({history}) {
     peer.on('stream', (stream) => {
       partnerVideo.current.srcObject = stream;
     });
-     
+
     // Add a disconnect method.
 
     peer.signal(callerSignal);
   }
-  
 
   // TODO: Send some feedback to the person who called.
   function rejectCall() {
     setReceivingCall(false);
   }
-  
+
   // Triggered only when user manually clicks on disconnect call button.
   function disconnectCall() {
     // Send an event to the server that user disconected.
@@ -212,14 +211,7 @@ function App({history}) {
   // Returns a list of people that we can call.
   // TODO: Add Styling
   function showUsersToCall() {
-    return (
-      <UserList 
-        users={users}
-        yourID={yourID}
-        handleClick={callPeer}
-        yourStream={stream}
-      />
-    );
+    return <UserList users={users} yourID={yourID} handleClick={callPeer} yourStream={stream} />;
   }
 
   let UserVideo;
@@ -232,16 +224,28 @@ function App({history}) {
   // TODO: Move it to a separate component.
   // Triggered by the listening to the hey event sent by the server.
   if (receivingCall) {
+    const header = `${caller.name} is calling you`;
     incomingCall = (
-      <div className="incoming-call-wrapper">
-        <h1>{caller.name} is calling you</h1>
-        <Button handleClick={acceptCall} variant="primary">
-          Accept
-        </Button>
-        <Button handleClick={rejectCall} variant="secondary">
-          Reject
-        </Button>
-      </div>
+      <Modal 
+        defaultOpen={true} 
+        closeOnDimmerClick={false} 
+        closeOnDocumentClick={false} 
+        basic 
+        size="small"
+      >
+        <Header icon="call" content={header} />
+        <Modal.Content>
+          <p>Would you like to accept the call?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={rejectCall} basic color="red" inverted>
+            <Icon name="remove" /> No
+          </Button>
+          <Button onClick={acceptCall} color="green" inverted>
+            <Icon name="checkmark" /> Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
     );
   }
 
@@ -261,34 +265,30 @@ function App({history}) {
   return (
     <Container>
       <Row className="video-section-wrapper">
-          {/* Different styling when host video is*/}
-          { (
-            <VideoWrapper className =
-              {`
+        {/* Different styling when host video is*/}
+        {
+          <VideoWrapper
+            className={`
                 host-video-wrapper
-                ${showPartnerVideo ? 'connected': '' }
-              `}>
-              {UserVideo}
-            </VideoWrapper>
-          )}
-          
+                ${showPartnerVideo ? 'connected' : ''}
+              `}
+          >
+            {UserVideo}
+          </VideoWrapper>
+        }
 
-          {showPartnerVideo && (
-            <VideoWrapper className="partner-video-wrapper">
-                <Video playsInline ref={partnerVideo} autoPlay />
-                <img onClick={() => disconnectCall()} className="call-cut" src={CallCut} />
-            </VideoWrapper>
-          )}
-        </Row>
-
-        { partnerDisconnected && (
-          <h2>User Disconnected...</h2>
+        {showPartnerVideo && (
+          <VideoWrapper className="partner-video-wrapper">
+            <Video playsInline ref={partnerVideo} autoPlay />
+            <img onClick={() => disconnectCall()} className="call-cut" src={CallCut} />
+          </VideoWrapper>
         )}
-      
+      </Row>
+
+      {partnerDisconnected && <h2>User Disconnected...</h2>}
 
       {showUsersToCall()}
       <Row>{incomingCall}</Row>
-
     </Container>
   );
 }
--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,32 +1,36 @@
-import React from 'react'
-import styled from 'styled-components';
-import Button from './Button';
+import React from 'react';
+import { Button, Card, Image } from 'semantic-ui-react';
 
-export default function UserList({users, yourID, handleClick, yourStream}) {
+export default function UserList({ users, yourID, handleClick, yourStream }) {
   return (
     <section className="user-list">
       <h1 className="align-center">Users online</h1>
-        <StyledList>
+      <Card.Group>
         {Object.keys(users).map((key) => {
+          const random = Math.random() * (100 - 0);
           const userName = users[key].name;
           // You dont want to call yourself for heaven sake.
           if (key === yourID || userName === null) {
             return null;
           }
-          // return <Button handleClick={() => handleClick(key, yourStream)}>Call {userName}</Button>;
           return (
-            <li>
-              <h6>{userName}</h6>
-              <Button handleClick={() => handleClick(key, yourStream)}>Call</Button>
-            </li>
-          )
+            <Card>
+              <Card.Content>
+                <Image floated="right" size="mini" src="https://react.semantic-ui.com/images/avatar/large/steve.jpg" />
+                <Card.Header>{userName}</Card.Header>
+                <Card.Meta>Available.</Card.Meta>
+              </Card.Content>
+              <Card.Content extra>
+                <div className="ui two buttons">
+                  <Button onClick={() => handleClick(key, yourStream)} basic color="green">
+                    Call
+                  </Button>
+                </div>
+              </Card.Content>
+            </Card>
+          );
         })}
-        </StyledList>
+      </Card.Group>
     </section>
-  )
+  );
 }
-
-const StyledList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-`;
